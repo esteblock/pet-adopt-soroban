31eb10c852c77e00f3648ec4835cf5cee4e1936d
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-12-07 17:46:16 -0300

    no fancy css

--- src/App.tsx
+++ src/App.tsx
@@ -1,10 +1,9 @@
 import React from 'react';
-import './App.css';
 import DogGrid from './components/DogGrid';
 
 function App() {
   return (
-    <div className="App">
+    <div>
         <DogGrid/>
     </div>
   );
