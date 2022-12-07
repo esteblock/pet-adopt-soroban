f63ea79283326c29e15986b64e96de86e7e08e89
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-12-07 14:55:58 -0300

    DogComponent. Solved #1

--- src/components/DogComponent.tsx
+++ src/components/DogComponent.tsx
@@ -1,5 +1,10 @@
-import React from 'react';
-import internal from 'stream';
+import * as React from 'react';
+import Card from '@mui/material/Card';
+import CardActions from '@mui/material/CardActions';
+import CardContent from '@mui/material/CardContent';
+import CardMedia from '@mui/material/CardMedia';
+import Button from '@mui/material/Button';
+import Typography from '@mui/material/Typography';
 
 export interface DogComponentProps {
     id: number,
@@ -9,13 +14,42 @@ export interface DogComponentProps {
     breed: string,
     location: string
 }
-export function DogComponent ({name, age, breed, id}: DogComponentProps){
-    return(
-        <div key={id}>
-            id: {id}
-            Name: {name}
-            Age: {age}
-            Breed: {breed}
-        </div>
-    )
+
+
+export function DogComponent (
+    {name, age, breed, id, location, picture}:
+        DogComponentProps){
+//     <div key={id}>
+//     id: {id}
+//     Name: {name}
+//     Age: {age}
+//     Breed: {breed}
+// </div>
+
+    return (
+    <Card key={id} sx={{ maxWidth: 345 }}>
+      <CardMedia
+        component="img"
+        alt="socttish-terrier"
+        height="345"
+        image={picture}
+      />
+      <CardContent>
+        <Typography gutterBottom variant="h5" component="div">
+        {name}
+        </Typography>
+        <Typography variant="body2" color="text.secondary">
+          <Typography>Age: {age}</Typography>
+          <Typography>Breed: {breed}</Typography>
+          <Typography>Location: {location}</Typography>
+          <Typography>Adopted: Not yet</Typography>
+        </Typography>
+      </CardContent>
+      <CardActions>
+        <Button size="small" variant="contained">
+            Adopt it now! ❤️
+        </Button>
+      </CardActions>
+    </Card>
+  );
 }
