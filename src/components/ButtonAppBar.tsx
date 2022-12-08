2496a304754b603c45a825d7ea9a3929ab229437
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-12-08 10:47:35 -0300

    make navbar sticky

--- src/components/ButtonAppBar.tsx
+++ src/components/ButtonAppBar.tsx
@@ -8,16 +8,15 @@ import IconButton from '@mui/material/IconButton';
 
 export default function ButtonAppBar() {
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
+
+      <AppBar position="sticky">
         <Toolbar>
-          
           <Typography variant="h6" component="div" sx={{ flexGrow: 1 }}>
             Pet Adopt Dapp in Soroban
           </Typography>
           <Button color="inherit">Connect Wallet</Button>
         </Toolbar>
       </AppBar>
-    </Box>
+  
   );
 }
