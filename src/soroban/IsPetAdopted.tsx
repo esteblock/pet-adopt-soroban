9d9b34dd5e70cb98998ebb9dc6ec3b1701d81d72
esteblock <estebaniglesiasmanriquez@gmail.com>
2022-12-15 17:36:18 -0300

    hardcoded chapter2

--- src/soroban/IsPetAdopted.tsx
+++ src/soroban/IsPetAdopted.tsx
@@ -2,11 +2,6 @@ import * as React from 'react';
 import { useSorobanReact } from '@soroban-react/core';
 import addresses from './addresses.json'
 import {useContractValue, useContractValueProps} from './useContractValue'
-import * as SorobanClient from 'soroban-client';
-let xdr = SorobanClient.xdr;
-import BigNumber from 'bignumber.js'
-import * as convert from '../utils/convert'
-import {numberIdentifier} from './identifiers'
 
 
 interface IsPetAdoptedProps {
@@ -19,22 +14,23 @@ export function IsPetAdopted ({id}: IsPetAdoptedProps){
     const sorobanContext = useSorobanReact()
     const useIsAdopted = (id: number): boolean => {
         
-        // useContractValue(Constants.CrowdfundId, 'deadline')
-       
-//bigNumberToI128
+        /*
+            TODO: delete hardcoded boolean and use useContractValue function
+        
+            let id_BN = new BigNumber(id)
+            let id_scval = convert.bigNumberToI128(id_BN.shiftedBy(0).decimalPlaces(0))
+    
+           
+            let numId = numberIdentifier(Buffer.from('1', 'hex'))
+    
+                console.log(" is adopted ", id, " : ", useContractValue({
+                                contractId: addresses.pet_adopt_id,
+                                method: 'adopted',
+                                params: [id_scval],
+                                sorobanContext: sorobanContext}))    
+        
+            */
 
-        let id_BN = new BigNumber(id)
-        let id_scval = convert.bigNumberToI128(id_BN.shiftedBy(0).decimalPlaces(0))
-
-       
-        let numId = numberIdentifier(Buffer.from('1', 'hex'))
-
-            console.log(" is adopted ", id, " : ", useContractValue({
-                            contractId: addresses.pet_adopt_id,
-                            method: 'adopted',
-                            params: [id_scval],
-                            sorobanContext: sorobanContext}))
-       
 
        
 
