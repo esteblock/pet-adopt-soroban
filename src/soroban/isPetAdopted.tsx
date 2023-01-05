ac165c72c5b3c9fdc9e682b71516c57c6b041fb1
esteblock <estebaniglesiasmanriquez@gmail.com>
2023-01-05 18:13:07 +0100

    reading the Blockchain with @soroban-react/contracts and @soroban-react/utils

--- src/soroban/isPetAdopted.tsx
+++ src/soroban/isPetAdopted.tsx
@@ -1,8 +1,8 @@
 import { SorobanContextType } from '@soroban-react/core';
 import addresses from './addresses.json'
-import {useContractValue, useContractValueProps} from '@soroban-react/contracts'
+import {useContractValue} from '@soroban-react/contracts'
 import BigNumber from 'bignumber.js'
-import {bigNumberToI128} from '@soroban-react/utils'
+import {numberToU32, scvalToBool} from '@soroban-react/utils'
 
 interface IsPetAdoptedProps {
     id: number,
@@ -11,21 +11,21 @@ interface IsPetAdoptedProps {
 
 
 export function isPetAdopted ({id, sorobanContext}: IsPetAdoptedProps){
-            let id_BN = new BigNumber(id)
-            let id_scval = bigNumberToI128(id_BN.shiftedBy(0).decimalPlaces(0))
-            let isAdopted = false
+//            let id_BN = BigNumber(id)
+            let id_scval = numberToU32(id)
+            let isAdopted_scval
+            let isAdopted 
             try{
-                isAdopted = useContractValue({
+                isAdopted_scval = useContractValue({
                     contractId: addresses.pet_adopt_id,
                     method: 'adopted',
                     params: [id_scval],
-                    sorobanContext: sorobanContext})
-            }
-            catch (error){
-                console.log("IsPetAdopted.tsx: error: ", error)
+                    sorobanContext: sorobanContext}).result
+                isAdopted = scvalToBool(isAdopted_scval)
+                console.log("pet id: ", id, " is adopted", isAdopted)
 
             }
-                
+            catch{}  
         return isAdopted
     }
 
